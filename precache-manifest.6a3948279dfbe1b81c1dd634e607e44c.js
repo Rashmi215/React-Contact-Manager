389060772b329e60cb6ea493dd4157c6e91d3e8f
Rashmi Singh <simplyrashmi02@gmail.com>
2018-12-20 12:08:10 +0530

    Updates

---
+++ precache-manifest.6a3948279dfbe1b81c1dd634e607e44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5251a6389631cd698f69",
+    "url": "/React-Contact-Manager/static/js/runtime~main.5251a638.js"
+  },
+  {
+    "revision": "05b301355bdc5ec52eb9",
+    "url": "/React-Contact-Manager/static/js/main.05b30135.chunk.js"
+  },
+  {
+    "revision": "a26fd84f504665aeb34a",
+    "url": "/React-Contact-Manager/static/js/1.a26fd84f.chunk.js"
+  },
+  {
+    "revision": "05b301355bdc5ec52eb9",
+    "url": "/React-Contact-Manager/static/css/main.267067e0.chunk.css"
+  },
+  {
+    "revision": "a26fd84f504665aeb34a",
+    "url": "/React-Contact-Manager/static/css/1.899f81e6.chunk.css"
+  },
+  {
+    "revision": "ff5289c384cd3aed1609d96909470b3e",
+    "url": "/React-Contact-Manager/index.html"
+  }
+];
